24f15cd6b32b031b2c095dd526f842556191aeeb
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-24 20:28:19 +0300

    Exercise: Parameterized Tests

--- tests/core.test.js
+++ tests/core.test.js
@@ -231,3 +231,15 @@ describe('canDriveWithParameterizedTests', () => {
         expect(canDrive(age, country)).toBe(result);
     })
 })
+
+describe("isPriceInRangeWithParameterizedTests", () => {
+    it.each([
+        { scenario: "price < min", price: -10, result: false},
+        { scenario: "price = min", price: 0, result: true},
+        { scenario: "price between min and max", price: 50, result: true},
+        { scenario: "price = max", price: 100, result: true},
+        { scenario: "price > max", price: 200, result: false},
+    ])("should return $result when $scenario", ({ price, result }) => {
+        expect(isPriceInRange(price, 0, 100)).toBe(result);
+    })
+})
