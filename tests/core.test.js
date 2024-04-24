63a7b0bb0b4cc053c95c8bdd2e8815210ec6208a
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-24 20:20:32 +0300

    Parameterized Tests

--- tests/core.test.js
+++ tests/core.test.js
@@ -214,3 +214,20 @@ describe('canDrive', () => {
         expect(canDrive(18, "UK")).toBe(true);
     })
 })
+
+describe('canDriveWithParameterizedTests', () => {
+    it("should return error for invalid country code", () => {
+        expect(canDrive(20, "FR")).toMatch(/invalid/i);
+    })
+
+    it.each([
+        { age: 15, country: "US", result: false },
+        { age: 16, country: "US", result: true },
+        { age: 17, country: "US", result: true },
+        { age: 16, country: "UK", result: false },
+        { age: 17, country: "UK", result: true },
+        { age: 18, country: "UK", result: true },
+    ])("should return $result for $age, $country", ( { age, country, result } ) => {
+        expect(canDrive(age, country)).toBe(result);
+    })
+})
