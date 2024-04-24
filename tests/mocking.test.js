988452a8989728416f07af68f6c92f723a0d84b9
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-24 22:40:15 +0300

    Exercise: Testing getShippingInfo

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,10 +1,8 @@
 
 import { vi,  it, expect, describe } from 'vitest'
-import { getPriceInCurrency } from '../src/mocking';
+import { getPriceInCurrency, getShippingInfo } from '../src/mocking';
 import { getExchangeRate } from '../src/libs/currency';
-
-// Hoisting (This line of code is pushed to the top of the file, regardless)
-vi.mock("../src/libs/currency");
+import { getShippingQuote } from '../src/libs/shipping';
 
 describe('test suite', () => {
     it("test case", () => {
@@ -57,6 +55,8 @@ describe('test suite', () => {
     })
 })
 
+// Hoisting (This line of code is pushed to the top of the file, regardless)
+vi.mock("../src/libs/currency");
 describe('test suite Exercise: Working with Mock Functions', () => {
     it("test case", () => {
         const sendText = vi.fn();
@@ -77,4 +77,27 @@ describe('getPriceInCurrency', () => {
 
         expect(price).toBe(15);
     })
-})
+});
+
+
+vi.mock("../src/libs/shipping")
+describe('getShippingInfo', () => {
+    it("should return shipping unavailable if quoute cannot be fetched", () => {
+        vi.mocked(getShippingQuote).mockReturnValue(null);
+        const result = getShippingInfo("London");
+
+        expect(result).toMatch(/unavailable/i);
+    });
+
+    it("should return shipping info if quote can be fetched", () => {
+        vi.mocked(getShippingQuote).mockReturnValue({ cost: 10, estimatedDays: 2 })
+
+        const result = getShippingInfo("London");
+
+        expect(result).toMatch("$10");
+        expect(result).toMatch(/2 days/i);
+        
+        //Or
+        expect(result).toMatch(/shipping cost: \$10 \(2 days\)/i)
+    })
+});
